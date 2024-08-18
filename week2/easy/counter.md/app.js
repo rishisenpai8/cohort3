fe6a7836c5f20709b2ffdc37797b8dd7cd1b624a
RishiSenpai <sharmarishi.it@gmail.com>
2024-08-18 20:12:21 +0530

    Doing easy assignment

---
+++ week2/easy/counter.md/app.js
@@ -0,0 +1,18 @@
+// Counter without setInterval
+// Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+
+// (Hint: setTimeout)
+
+
+let counter = 0;
+
+function count(){
+    console.log(counter);
+    counter++;
+
+    setTimeout(count, 1000);
+}
+
+
+count()
+
